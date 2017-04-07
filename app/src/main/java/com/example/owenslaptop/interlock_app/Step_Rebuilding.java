98491a6d535422d5ba4293fe5121bbb4215c6752
odog99 <odog.j1633@gmail.com>
2017-04-07 11:16:42 -0400

    Added a few comments as well as the pages that we are going to be starting on

--- app/src/main/java/com/example/owenslaptop/interlock_app/Step_Rebuilding.java
+++ app/src/main/java/com/example/owenslaptop/interlock_app/Step_Rebuilding.java
@@ -3,21 +3,11 @@ package com.example.owenslaptop.interlock_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeScreen extends AppCompatActivity {
-
-    /*
-
-    Names: Ethan McIntyre, Owen Brook & Peter Lewis
-
-    Description:
-
-    Date Started: April 7th, 2017
-
-     */
+public class Step_Rebuilding extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_step__rebuilding);
     }
 }
