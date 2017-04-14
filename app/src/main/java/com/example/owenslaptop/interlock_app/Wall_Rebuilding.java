4d625ffa18e1451a592926c33e95bf979ca2cac0
Peter Lewis <peter.chris.lewis@gmail.com>
2017-04-13 22:25:08 -0400

    added a button to go to the home window in the Wall_Rebuilding window

--- app/src/main/java/com/example/owenslaptop/interlock_app/Wall_Rebuilding.java
+++ app/src/main/java/com/example/owenslaptop/interlock_app/Wall_Rebuilding.java
@@ -1,7 +1,9 @@
 package com.example.owenslaptop.interlock_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Wall_Rebuilding extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Wall_Rebuilding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall__rebuilding);
+    }
+
+    public void homeButtonClicked(View view){
+        startActivity(new Intent(this, HomeScreen.class));
+    }
+    public static void create(){
+        new Wall_Rebuilding().h();
+    }
+    private void h(){
+
     }
 }
