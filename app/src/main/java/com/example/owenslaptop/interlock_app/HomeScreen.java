2c87b8e049750bf013ba480ba8a101015f4f10bf
odog99 <odog.j1633@gmail.com>
2017-05-01 17:35:56 -0400

    adding components to the step_rebuilding page as well as button to main to access for testing reasons

--- app/src/main/java/com/example/owenslaptop/interlock_app/HomeScreen.java
+++ app/src/main/java/com/example/owenslaptop/interlock_app/HomeScreen.java
@@ -1,7 +1,10 @@
 package com.example.owenslaptop.interlock_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -19,5 +22,17 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        Button btn = (Button)findViewById(R.id.stepRebuildBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this, Step_Rebuilding.class));
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/owenslaptop/interlock_app/Step_Rebuilding.java
+++ app/src/main/java/com/example/owenslaptop/interlock_app/Step_Rebuilding.java
@@ -2,12 +2,105 @@ package com.example.owenslaptop.interlock_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class Step_Rebuilding extends AppCompatActivity {
+
+    /*
+
+    Coded by: Owen Brook
+
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step__rebuilding);
+
+        //setting up the GUI components
+        final Button nextBtn = (Button) findViewById(R.id.nextBtn);
+        final CheckBox straightCB = (CheckBox) findViewById(R.id.straightCB);
+        final CheckBox curvedCB = (CheckBox) findViewById(R.id.curvedCB);
+        final Spinner sizeJobSp = (Spinner) findViewById(R.id.sizeOfJobSp);
+        final Spinner sizeJob2Sp = (Spinner) findViewById(R.id.sizeOfJob2Sp);
+        final Spinner amountShiftSp = (Spinner) findViewById(R.id.amountBaseShiftSp);
+        final TextView errorTV = (TextView) findViewById(R.id.errorTV);
+
+        //creating the arrays to hold the spinner objects
+        final String[] sizeArr = {"Size of the job (l)", "Shorter", "Average", "Longer"};
+        final String[] size2Arr = {"Size of the job (h)", "Lower", "Average", "Higher", "Over 4 feet"};
+        final String[] baseShiftArr = {"Amount of base shift", "None", "Lower"};
+
+        //setting the options to the spinners
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, sizeArr);
+        sizeJobSp.setAdapter(adapter1);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, size2Arr);
+        sizeJob2Sp.setAdapter(adapter2);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, baseShiftArr);
+        amountShiftSp.setAdapter(adapter3);
+
+        //setting up the button to get and check input when clicked
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //this will get and store the variables
+
+                //setting up the variables for storing the input
+                boolean straight, curved;
+                String size, size2, amountShift;
+
+                //seeing if we have input from the spinners
+                size = sizeJobSp.getSelectedItem().toString();
+                size2 = sizeJob2Sp.getSelectedItem().toString();
+                amountShift = amountShiftSp.getSelectedItem().toString();
+
+                String sizeVal = sizeArr[0].toString();
+                String size2Val = size2Arr[0].toString();
+                String amountShiftVal = baseShiftArr[0].toString();
+
+                if(size.equals(sizeVal)){
+                    //one of the inputs are incorrect
+                    errorTV.setText("Please select a option from the 'Size of the job (l)' drop down");
+                }
+                else if(size2.equals(size2Val)){
+                    //one of the inputs are incorrect
+                    errorTV.setText("Please select a option from the 'Size of the job (h)' drop down");
+                }
+                else if(amountShift.equals(amountShiftVal)){
+                    //one of the inputs are incorrect
+                    errorTV.setText("Please select an option from the 'Amount of base Shift' drop down");
+                }
+                else{
+                    //we are good to look at the check boxes
+                    //seeing if both boxes are checked
+                    straight = straightCB.isChecked();
+                    curved = curvedCB.isChecked();
+                    if(straight && curved){
+                        //both are checked
+                        errorTV.setText("Both check boxes cannot be selected");
+                    }
+                    else if(!straight && !curved){
+                        //both are not checked
+                        errorTV.setText("Please check one of the check boxes");
+                    }
+                    else{
+                        //we are good to go on to the next page
+                        errorTV.setText("");
+
+                        //moving to the next page
+
+                    }
+                }
+            }
+        });
     }
 }
