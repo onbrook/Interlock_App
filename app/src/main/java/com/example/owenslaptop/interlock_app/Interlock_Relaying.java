5ca45fcb1ed9a48c46d5b7c02d4ab96016fd04f1
Ethan <ethansm10@gmail.com>
2017-05-02 14:36:25 -0400

    converted interlock relaying pages to RelativeLayout

--- app/src/main/java/com/example/owenslaptop/interlock_app/Interlock_Relaying.java
+++ app/src/main/java/com/example/owenslaptop/interlock_app/Interlock_Relaying.java
@@ -2,6 +2,8 @@ package com.example.owenslaptop.interlock_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Interlock_Relaying extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class Interlock_Relaying extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interlock__relaying_layout);
+        final EditText length = (EditText)findViewById(R.id.lenInput);
+        final EditText width = (EditText)findViewById(R.id.widInput);
     }
 }
