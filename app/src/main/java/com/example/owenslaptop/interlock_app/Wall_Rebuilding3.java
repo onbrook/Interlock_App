d73de6950c604abbdbaae81b22d93145de755e10
Peter Lewis <peter.chris.lewis@gmail.com>
2017-05-15 12:48:19 -0400

    added a conformation/estimation page for the Wall rebuilding pages

--- app/src/main/java/com/example/owenslaptop/interlock_app/Wall_Rebuilding3.java
+++ app/src/main/java/com/example/owenslaptop/interlock_app/Wall_Rebuilding3.java
@@ -7,7 +7,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import static com.example.owenslaptop.interlock_app.ViewValidity.areViewsValid;
 import static com.example.owenslaptop.interlock_app.ViewValidity.updateViewValidity;
@@ -19,11 +21,14 @@ import static com.example.owenslaptop.interlock_app.ViewValidity.updateViewValid
 
 public class Wall_Rebuilding3 extends AppCompatActivity {
     Spinner baseShiftSpinner;
-    CheckBox rootsCheckBox;
+    SeekBar rootsSeekBar;
+    SeekBar plantsSeekBar;
     CheckBox glueCheckBox;
     CheckBox clipsCheckBox;
-    CheckBox plantsCheckBox;
+    int rootsNum = 0;
+    int plantsNum = 0;
     View[] views = new View[1];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +45,12 @@ public class Wall_Rebuilding3 extends AppCompatActivity {
         }
         //views
         baseShiftSpinner = (Spinner) findViewById(R.id.baseShift_spinner);
-        rootsCheckBox = (CheckBox) findViewById(R.id.roots_CheckBox);
+        rootsSeekBar = (SeekBar) findViewById(R.id.roots_seek_bar);
+        final TextView rootsTV = (TextView) findViewById(R.id.roots_tv);
+        plantsSeekBar = (SeekBar) findViewById(R.id.plants_seek_bar);
+        final TextView plantsTV = (TextView) findViewById(R.id.plants_tv);
         glueCheckBox = (CheckBox) findViewById(R.id.glue_CheckBox);
         clipsCheckBox = (CheckBox) findViewById(R.id.clips_CheckBox);
-        plantsCheckBox = (CheckBox) findViewById(R.id.plants_CheckBox);
         views[0] = baseShiftSpinner;
         //item change listener
         baseShiftSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -62,6 +69,67 @@ public class Wall_Rebuilding3 extends AppCompatActivity {
                     updateViewValidity(views);
             }
         });
+        //SeekBar listeners
+        rootsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {/* do nothing*/}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {/* do nothing*/}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // update angleTV
+                if(progress<=12){
+                    seekBar.setProgress(0);
+                    rootsTV.setText("None");
+                    rootsNum = 0;
+                }else if(progress<=50){
+                    seekBar.setProgress(33);
+                    rootsTV.setText("Some");
+                    rootsNum = 1;
+                }else if(progress<=88){
+                    seekBar.setProgress(66);
+                    rootsTV.setText("A moderate amount");
+                    rootsNum = 2;
+                }else{
+                    seekBar.setProgress(100);
+                    rootsTV.setText("Lots");
+                    rootsNum = 3;
+                }
+            }
+        });
+        plantsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {/* do nothing*/}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {/* do nothing*/}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // update angleTV
+                if(progress<=12){
+                    seekBar.setProgress(0);
+                    plantsTV.setText("None");
+                    plantsNum = 0;
+                }else if(progress<=50){
+                    seekBar.setProgress(33);
+                    plantsTV.setText("Some");
+                    plantsNum = 1;
+                }else if(progress<=88){
+                    seekBar.setProgress(66);
+                    plantsTV.setText("A moderate amount");
+                    plantsNum = 2;
+                }else{
+                    seekBar.setProgress(100);
+                    plantsTV.setText("Lots");
+                    plantsNum = 3;
+                }
+            }
+        });
     }
 
     public void fabClicked(View fab) {
@@ -72,7 +140,8 @@ public class Wall_Rebuilding3 extends AppCompatActivity {
             intent.setClass(getApplicationContext(), Wall_Rebuilding_Estimation.class);
             //extras--for passing data
             intent.putExtra("baseShiftIndex", baseShiftSpinner.getSelectedItemPosition());
-            intent.putExtra("rootsChecked", rootsCheckBox.isChecked());
+            intent.putExtra("rootsNum", rootsNum);
+            intent.putExtra("plantsNum", plantsNum);
             intent.putExtra("glueChecked", glueCheckBox.isChecked());
             intent.putExtra("clipsChecked", clipsCheckBox.isChecked());
             startActivity(intent);
