57657d425cebb937bdf7fac3ca4e925da21d099d
odog99 <odog.j1633@gmail.com>
2017-05-30 00:01:09 -0400

    changed the files on my end to fix issues I was having.  Also changed the pages so it is now working properly to switch between

--- app/src/main/java/com/oep/interlock_app/HomeScreen.java
+++ app/src/main/java/com/oep/interlock_app/HomeScreen.java
@@ -23,6 +23,8 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        setTitle("Home");
 /*
         //check if the user is new and if so, go to correct activity
         EstimationSheet estimationSheet = new EstimationSheet(EstimationSheet.ID_NOT_APPLICABLE, this);
@@ -39,14 +41,6 @@ public class HomeScreen extends AppCompatActivity {
             else
                 startActivity(new Intent(getApplicationContext(), EnterDatabaseIdActivity.class));*/
 
-        setContentView(R.layout.activity_home_screen);
-        setTitle("Home");
-        Button stepBtn = (Button)findViewById(R.id.stepRebuildBtn);
-        Button wallBtn = (Button)findViewById(R.id.wallRebuildBtn);
-        Button relayBtn = (Button)findViewById(R.id.relayBtn);
-        Button jointBtn = (Button)findViewById(R.id.jointFillBtn);
-        Button sealBtn = (Button)findViewById(R.id.cleanSealBtn);
-
         //setting up the buttons
         Button estimateBtn = (Button)findViewById(R.id.estimatePageBtn);
 
@@ -55,35 +49,7 @@ public class HomeScreen extends AppCompatActivity {
         estimateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this, Step_Rebuilding.class));
-            }
-        });
-
-        wallBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this, Wall_Rebuilding.class));
-            }
-        });
-
-        relayBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this, Interlock_Relaying.class));
-            }
-        });
-
-        jointBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this, Joint_Fill.class));
-            }
-        });
-
-        sealBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeScreen.this, CleaningSealing.class));
+                startActivity(new Intent(HomeScreen.this, EstimationPage.class));
             }
         });
 
