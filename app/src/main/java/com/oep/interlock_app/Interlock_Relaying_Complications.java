580e0852346b2aff2ed89192c6778e39822443c1
Ethan <ethansm10@gmail.com>
2017-05-31 10:36:56 -0400

    Unified input methods for interlock relaying pages, implemented back button.

--- app/src/main/java/com/oep/interlock_app/Interlock_Relaying_Complications.java
+++ app/src/main/java/com/oep/interlock_app/Interlock_Relaying_Complications.java
@@ -5,47 +5,168 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
-
+import android.widget.TextView;
+import android.view.MenuItem;
 import com.oep.owenslaptop.interlock_app.R;
+import org.w3c.dom.Text;
+import static com.google.example.easypermissions.R.styleable.MenuItem;
 
 public class Interlock_Relaying_Complications extends AppCompatActivity {
 
-    public static boolean pageCheck(Spinner size){
-        //clearing all outlines
-        ViewValidity.removeOutline(size);
+    public static String sunkOut, paveSizeOut, weedOut, fillOut;
 
-        if(String.valueOf(size.getSelectedItem()).equals("Size of pavers")){
-            ViewValidity.setupOutline(size);
-            return false;
-        }else{
-            return true;
-        }
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interlock_relaying_complications);
+        //setting up the back button
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (NullPointerException npex) {
+            try {
+                getActionBar().setDisplayHomeAsUpEnabled(true);
+            } catch (NullPointerException ex) {
+                //back button not supported
+            }
+        }
         final SeekBar sunkAmtSlider = (SeekBar)findViewById(R.id.sunkAmtSlider);
-        final Spinner paverSize = (Spinner)findViewById(R.id.paverSizeSpin);
+        final SeekBar paverSlider = (SeekBar)findViewById(R.id.paverSlider);
         final FloatingActionButton nextBtn = (FloatingActionButton)findViewById(R.id.nextBtn);
+        final TextView sunkAmtTxt = (TextView)findViewById(R.id.sunkDisplay);
+        final TextView paversTxt = (TextView)findViewById(R.id.paversDisplay);
+        final SeekBar jointFillSlider = (SeekBar)findViewById(R.id.jointFillSlider);
+        final TextView jointFillTxt = (TextView)findViewById(R.id.jointFillDisplay);
+        final TextView weedTxt = (TextView)findViewById(R.id.weedDisplay);
+        final SeekBar weedSlider = (SeekBar)findViewById(R.id.weedSlider);
+
+        jointFillSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress == 0){
+                    jointFillTxt.setText("Not very hard");
+                }else if(progress == 1){
+                    jointFillTxt.setText("Somewhat hard");
+                }else if(progress == 2){
+                    jointFillTxt.setText("Fairly Hard");
+                }else if(progress == 3){
+                    jointFillTxt.setText("Moderately Hard");
+                }else{
+                    jointFillTxt.setText("Very hard");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        weedSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress == 0){
+                    weedTxt.setText("No weeds");
+                }else if(progress == 1){
+                    weedTxt.setText("Some weeds");
+                }else if(progress == 2){
+                    weedTxt.setText("Slightly weedy");
+                }else if(progress == 3){
+                    weedTxt.setText("Moderately weedy");
+                }else{
+                    weedTxt.setText("Very weedy");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        paverSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress == 0 ){
+                    paversTxt.setText("Small Paver Size");
+                }else if(progress == 1){
+                    paversTxt.setText("Medium Paver Size");
+                }else{
+                    paversTxt.setText("Large Paver Size");
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        sunkAmtSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress == 0) {
+                    sunkAmtTxt.setText("Hasn't sunk");
+                } else if (progress == 1) {
+                    sunkAmtTxt.setText("Has sunk slightly");
+                } else if (progress == 2) {
+                    sunkAmtTxt.setText("Has sunk a moderate amount");
+                } else if (progress == 3){
+                    sunkAmtTxt.setText("Has sunk a great amount");
+                } else if(progress == 4){
+                    sunkAmtTxt.setText("Severely sunken");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Boolean check = pageCheck(paverSize);
-                //add function to check for current layout
-                if(check == true){
                     Intent nextPg = getIntent();
+                    sunkOut = String.valueOf(sunkAmtTxt.getText());
+                    paveSizeOut = String.valueOf(paversTxt.getText());
+                    weedOut = String.valueOf(weedTxt.getText());
+                    fillOut = String.valueOf(jointFillTxt.getText());
                     nextPg.setClass(getApplicationContext(), Interlock_Relaying_ToJob.class);
-                    //save info to file here
                     startActivity(nextPg);
-                }else{
-                    System.out.print("Error");
-                }
+
             }
         });
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        onBackPressed();
+        return true;
     }
 }
--- app/src/main/java/com/oep/interlock_app/Interlock_Relaying_Estimate.java
+++ app/src/main/java/com/oep/interlock_app/Interlock_Relaying_Estimate.java
@@ -2,8 +2,12 @@ package com.oep.interlock_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.oep.owenslaptop.interlock_app.R;
+
+import org.w3c.dom.Text;
 
 public class Interlock_Relaying_Estimate extends AppCompatActivity {
 
@@ -11,5 +15,34 @@ public class Interlock_Relaying_Estimate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interlock__relaying__estimate);
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (NullPointerException npex) {
+            try {
+                getActionBar().setDisplayHomeAsUpEnabled(true);
+            } catch (NullPointerException ex) {
+                //back button not supported
+            }
+        }
+        final TextView output = (TextView)findViewById(R.id.output);
+
+        output.setText("Width: " + Interlock_Relaying.widthOut + "\n" +
+                "Length: " + Interlock_Relaying.lengthOut + "\n" +
+                "Pattern: " + Interlock_Relaying.ptrnOut + "\n" +
+                "Shape: " + Interlock_Relaying.shapeOut + "\n" +
+                Interlock_Relaying.edgeOut + "\n" +
+                "The interlock... " + Interlock_Relaying_Complications.sunkOut + "\n" +
+                "The interlock is... " + Interlock_Relaying_Complications.paveSizeOut + "\n" +
+                "There are... " + Interlock_Relaying_Complications.weedOut + "\n" +
+                "The joint fill is... " + Interlock_Relaying_Complications.fillOut + "\n" +
+                Interlock_Relaying_ToJob.locOut + "\n" +
+                Interlock_Relaying_ToJob.moveOut + "\n" +
+                Interlock_Relaying_ToJob.accOut);
+                //estimate here
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        onBackPressed();
+        return true;
     }
 }
